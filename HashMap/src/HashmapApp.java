dcd1fd329b109a3899446174a8a1b0dace9c97a1
Mena Abouelnasr <menabouelnasr3@gmail.com>
2015-08-07 16:36:22 -0400

    Commit 2

--- HashMap/src/HashmapApp.java
+++ HashMap/src/HashmapApp.java
@@ -1,17 +1,20 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class HashmapApp 
 
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException 
 	{
-		/*HashMap<String,String> map = new HashMap<String,String>();
-		map.put("cat", "map"); // 1) key 2) sounds
-		map.put("dog", "woof");
-		System.out.println("map = " + map);
-		System.out.println("A cat says..."+ map.get("cat: "));
-		*/
 		String choice="y", spell;
 		int num;
 		
@@ -24,29 +27,48 @@ public class HashmapApp
 		myMap.put(4, "four");
 		myMap.put(5, "five");
 		
+		
+		//get a filename
+				System.out.println((System.getProperty("user.dir") + File.separatorChar +"mybooks.txt"));
+				String filename = (System.getProperty("user.dir") + File.separatorChar +"mybooks.txt");
+				
+				System.out.println(Paths.get("c:\\myfolder\\myfile.txt"));
+				PrintWriter writer;
+				writer = new PrintWriter(new File(filename));
+				FileWriter fstream  = new FileWriter("mybooks.txt"); ; 
+				BufferedWriter out = new BufferedWriter(fstream); 
+				
 		while (choice.equalsIgnoreCase("y"))
 		{
 			System.out.println("Enter a number: ");
 			num=keyboard.nextInt();
 			
+			
 			if(myMap.containsKey(num))
 			{
-				System.out.println("here");
-				System.out.println("Response: You entered "+ myMap.get(num) + "" );
+				writer.println(myMap.get(num));
 			}
 			else
 			{
 				System.out.println("You have entered a number which is not in the map.");
 				System.out.println("Please provide the spelling of the number.");
 				spell=keyboard.next();
-				myMap.put(num,spell);
+				writer.println(myMap.put(num,spell));
 				String value = myMap.get(num);
-				System.out.println("Response: You entered "+ value + ".");
 			}
-	
-			System.out.println("Would you like to try again? (Y/N)");
+			
+			System.out.println("Would you like enter another number? (Y/N)");
 			choice=keyboard.next();
 		}
-	}
+			writer.close(); //closes the PrintWriter
+			Iterator<Entry<Integer, String>> it = myMap.entrySet().iterator(); //iterates through the hashmap to print
+			while (it.hasNext())
+			{
+				Map.Entry<Integer, String> values = it.next(); //gets the keys and values at each point in the hashmap
+				out.write(values.getKey() + "   " +values.getValue() + "\n"); //writes each line of the hashmap to the file
+			}
+			out.close(); // closes the BufferedWriter
+		}
+	
 
 }
